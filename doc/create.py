4e34406470a0314525d9444e38de8c8a59f682f3
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-06-17 13:51:32 -0400

    address reviewer comments
    Change-Id: I41831decb7b703d5ec68b4ba52f2cd2c72e07fca

--- doc/create.py
+++ doc/create.py
@@ -1,15 +1,10 @@
-# (C) Copyright 2011- ECMWF.
-#
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-# In applying this licence, ECMWF does not waive the privileges and immunities
-# granted to it by virtue of its status as an intergovernmental organisation
-# nor does it submit to any jurisdiction.
+#!/usr/bin/env python
 
 """
 Extract rst documentation for a given list of ecBuild macros for use with Sphinx.
 """
 
+from __future__ import print_function
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from os import environ, path, makedirs
 
