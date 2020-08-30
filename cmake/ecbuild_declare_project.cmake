47f54e4bfcd55666964d43d919199a5066fbf9b3
Mark J. Olah <markjolah@users.noreply.github.com>
2020-08-30 14:19:19 -0600

    spelling

--- cmake/ecbuild_declare_project.cmake
+++ cmake/ecbuild_declare_project.cmake
@@ -118,7 +118,7 @@ if( NOT ${PROJECT_NAME}_DECLARED )
   # install dirs for this project
 
   # Use defaults unless values are already present in cache
-  include(GNUInstalDirs)
+  include(GNUInstallDirs)
   if( NOT INSTALL_BIN_DIR )
     set( INSTALL_BIN_DIR bin )
   endif()
